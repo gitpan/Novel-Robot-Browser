6dddeb6007dad10644a5dda6b4373da053250143
潘蓝兰 <abbypan@gmail.com>
2013-02-28 15:41:53 +0000

    Import of ABBYPAN/Novel-Robot-Browser-0.06 from CPAN.
    gitpan-cpan-distribution: Novel-Robot-Browser
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         ABBYPAN/Novel-Robot-Browser-0.06.tar.gz
    gitpan-cpan-author:       ABBYPAN
    gitpan-cpan-maturity:     released

--- lib/Novel/Robot/Browser.pm
+++ lib/Novel/Robot/Browser.pm
@@ -1,15 +1,17 @@
-# ABSTRACT: 自动化获取网页内容，并解码为unicode
+#  ABSTRACT: 自动化获取网页内容，并解码为unicode
 package Novel::Robot::Browser;
 
 use strict;
 use warnings;
 use utf8;
 
+our $VERSION = '0.02';
+
 use Encode::Detect::CJK qw/detect/;
 use Encode;
+use HTTP::Tiny;
 use IO::Uncompress::Gunzip qw(gunzip $GunzipError);
 use Moo;
-use LWP::UserAgent;
 
 has retry => ( is => 'rw', default => sub {3}, );
 
@@ -20,18 +22,22 @@ sub get_url_ref {
 
     my $response;
     for my $i ( 1 .. $self->{retry} ) {
-        $response =
-              $post_vars
-            ? $self->{http}->post( $url, $post_vars )
-            : $self->{http}->get($url);
-        last if ( $response->is_success );
+
+        if ($post_vars) {
+            my $post_data = $self->make_post_data($post_vars);
+            $response = $self->{http}->request( 'POST', $url, $post_data );
+        }
+        else {
+            $response = $self->{http}->get($url);
+        }
+
+        last if ( $response->{success} );
     } ## end for my $i ( 1 .. $self->...)
 
-    return unless ( $response->is_success );
+    return unless ( $response->{success} );
 
     my $html;
-    my $c = $response->content;
-    gunzip \$c => \$html;
+    gunzip \$response->{content} => \$html;
 
     my $charset = detect($html);
     $html = decode( $charset, $html, Encode::FB_XMLCREF );
@@ -41,23 +47,37 @@ sub get_url_ref {
 
 sub init_browser {
     my ($self) = @_;
-
-    my $browser = LWP::UserAgent->new( keep_alive => 1 );
-    $browser->cookie_jar( {} );
-    push @{ $browser->requests_redirectable }, 'POST';
-
     my %browser_opt = (
-        'Accept'          => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset'  => 'gb2312,utf-8;q=0.7,*;q=0.7',
-        'Accept-Encoding' => "gzip, deflate",
-        'User-Agent' => 'Moozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; MALC)',
-        'Accept-Language' => "zh,zh-cn;q=0.8,zh-tw;q=0.6,en-us;q=0.4,en;q=0.2",
+        default_headers => {
+            'Accept'          => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset'  => 'gb2312,utf-8;q=0.7,*;q=0.7',
+            'Accept-Encoding' => "gzip, deflate",
+            'Connection'      => 'keep-alive',
+            'User-Agent' =>
+                'Moozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; MALC)',
+            'Accept-Language' => "zh,zh-cn;q=0.8,zh-tw;q=0.6,en-us;q=0.4,en;q=0.2",
+        },
     );
-    $browser->default_header( $_ => $browser_opt{$_} ) for keys(%browser_opt);
+    my $http = HTTP::Tiny->new(%browser_opt);
 
-    return $browser;
+    return $http;
 } ## end sub init_browser
 
-no Moo;
+sub make_post_data {
+    my ( $self, $post_vars ) = @_;
 
+    my @params;
+    while ( my @pair = each %$post_vars ) {
+        push @params, join( "=", @pair );
+    }
+
+    my $data = {
+        content => join( "&", @params ),
+        headers => { 'content-type' => 'application/x-www-form-urlencoded' }
+    };
+
+    return $data;
+} ## end sub make_post_data
+
+no Moo;
 1;
